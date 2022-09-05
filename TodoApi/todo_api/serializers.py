350e10ddba9242e4e966a4131ff4998f1f3261f2
Lokesh <67707359+LokeshJangid01@users.noreply.github.com>
2022-09-05 15:22:05 +0530

    this is first app in API MODE

---
+++ TodoApi/todo_api/serializers.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+from rest_framework import serializers
+
+from .models import *
+
+class TaskSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Task
+        
+        fields = '__all__'
+        
+        
